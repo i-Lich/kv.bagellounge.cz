db0553ad26f695f121313c7c9eca74a7c19766a1
ilich <st.rowdy@gmail.com>
2020-05-23 09:37:17 +0300

    +kv.bagellounge.cz

--- kv.bagellounge.cz/js/script.js
+++ kv.bagellounge.cz/js/script.js
@@ -27,3 +27,31 @@
 //
 // ibg();
 // //end ibg
+
+$(function(){
+    $(".menu-more").click(function () {
+        $('#lsp-block-tree').toggleClass('open');
+    });
+    $(".recommendation-more").click(function () {
+        $('.lsp-js-recommendation-items').toggleClass('open');
+    });
+    $(".cart-more").click(function () {
+        $('.lsp-cart-items-list').toggleClass('open');
+        $('.lsp-info-message-bottom').toggleClass('open');
+    });
+});
+
+window.jStoreEvents = window.jStoreEvents ? window.jStoreEvents : [];
+jStoreEvents.push(['pageChanged', null, function (data) {
+    $('#show-nav').removeClass('active');
+    $('.jstore-block-search').removeClass('open');
+    $('.lsp-block-terminalinfo').removeClass('open');
+    $('#lsp-block-userinfo').removeClass('open');
+    // $('.nav-menu').removeClass('open');
+    $('div.lsp-cart-items-list').removeClass('open');
+    $('.lsp-info-message-bottom').removeClass('open');
+    $('#lsp-block-gift').removeClass('open');
+    $('#lsp-block-happy-hour').removeClass('open');
+    $('#lsp-block-recommendation').removeClass('open');
+}]);
+
